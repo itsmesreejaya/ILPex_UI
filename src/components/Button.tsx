ad830f38e9e22a655d26dc60ae53f7b26a5a6c0b
Sreejaya <itsmesreejaya@gmail.com>
2024-02-20 12:27:08 +0530

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, TouchableOpacity,Text,StyleSheet, Button } from "react-native";
+
+
+
+const ButtonComponent = () =>{
+
+    return(
+        <View>
+            <Button title="Login" />
+      </View>
+    )
+}
+
+export default ButtonComponent;
